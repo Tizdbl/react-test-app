6f269382dc15d3413a40cb25805a13aec5f7aa4f
Moca <lubenicakonsalting@gmail.com>
2024-03-20 17:18:13 +0100

    buttons to make textfields and radio buttons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [textFields, setTextFields] = useState([]);
+  const [radioButtons, setRadioButtons] = useState([]);
+
+  const handleButtonClick = () => {
+    setTextFields([...textFields, '']);
+  };
+  const handleRadioButtonButtonClick = () => {
+    setRadioButtons([...radioButtons, '']);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <div>
+          <button onClick={handleButtonClick}>Add Text Field</button>
+          <button onClick={handleRadioButtonButtonClick}>Add Radio Button</button>
+        </div>
+        <div>
+          {textFields.map((value, index) => (
+              <input
+                  key={index}
+                  type="text"
+                  value={value}
+                  onChange={(e) => {
+                    const updatedTextFields = [...textFields];
+                    updatedTextFields[index] = e.target.value;
+                    setTextFields(updatedTextFields);
+                  }}
+              />
+          ))}
+        </div>
+        <div>
+          {radioButtons.map((value, index) => (
+              <input
+                  key={index}
+                  type="radio"
+                  name="radioButtonGroup"
+                  value={value}
+              />
+          ))}
+        </div>
+      </div>
   );
 }
 
